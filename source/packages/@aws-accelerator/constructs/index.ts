a97dc086368367275501e9d25680c385228f9afd
Randy Domingo <domrandy@amazon.com>
2021-10-01 12:46:09 -0700

    chore(docs): Add organizational-unit-lookup to exports

--- source/packages/@aws-accelerator/constructs/index.ts
+++ source/packages/@aws-accelerator/constructs/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/aws-organizations/organizational-unit';
+export * from './lib/aws-organizations/organizational-unit-lookup';
 export * from './lib/aws-organizations/root-organizational-unit';
