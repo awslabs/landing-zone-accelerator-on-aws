f0b25595198f3b966b33e35315228fb36ec406b5
Bo Lechangeur <lechange@amazon.com>
2022-01-31 23:26:33 +0000

    feat(vpc): add support to delete the default vpcs in governed accounts and regions

--- source/packages/@aws-accelerator/constructs/lib/aws-ec2/delete-default-vpc/index.ts
+++ source/packages/@aws-accelerator/constructs/lib/aws-ec2/delete-default-vpc/index.ts
@@ -11,7 +11,24 @@
  *  and limitations under the License.
  */
 
-// import { throttlingBackOff } from '@aws-accelerator/utils';
+import { throttlingBackOff } from '@aws-accelerator/utils';
+import * as console from 'console';
+import {
+  DeleteInternetGatewayCommand,
+  DetachInternetGatewayCommand,
+  DeleteNetworkAclCommand,
+  DeleteRouteCommand,
+  DeleteSecurityGroupCommand,
+  DeleteSubnetCommand,
+  DeleteVpcCommand,
+  EC2Client,
+  paginateDescribeInternetGateways,
+  paginateDescribeNetworkAcls,
+  paginateDescribeRouteTables,
+  paginateDescribeSecurityGroups,
+  paginateDescribeSubnets,
+  paginateDescribeVpcs,
+} from '@aws-sdk/client-ec2';
 
 /**
  * delete-default-vpc - lambda handler
@@ -19,16 +36,163 @@
  * @param event
  * @returns
  */
+
 export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent): Promise<
   | {
-      PhysicalResourceId: string | undefined;
-      Status: string;
-    }
+    PhysicalResourceId: string | undefined;
+    Status: string;
+  }
   | undefined
 > {
+  // Retrieve operating region that stack is ran
+  const region = event.ResourceProperties['region'];
+  const ec2Client = new EC2Client({ region: region });
+  const defaultVpcIds: string[] = [];
+
   switch (event.RequestType) {
     case 'Create':
     case 'Update':
+      console.log(`Starting - Deletion of default VPC and associated resources in ${region}`);
+
+      // Retrieve default VPC(s)
+      for await (const page of paginateDescribeVpcs(
+        { client: ec2Client },
+        { Filters: [{ Name: 'is-default', Values: [`true`] }] },
+      )) {
+        for (const vpc of page.Vpcs ?? []) {
+          if (vpc.VpcId) {
+            defaultVpcIds.push(vpc.VpcId);
+          }
+        }
+      }
+      console.log(`List of VPCs: `, defaultVpcIds);
+      if (defaultVpcIds.length == 0) {
+        console.warn('No default VPCs detected');
+        return {
+          PhysicalResourceId: `delete-default-vpc`,
+          Status: 'SUCCESS',
+        };
+      } else {
+        console.warn('Default VPC Detected');
+      }
+
+      // Retrieve and detach, delete IGWs
+      for (const vpcId of defaultVpcIds) {
+        for await (const page of paginateDescribeInternetGateways(
+          { client: ec2Client },
+          { Filters: [{ Name: 'attachment.vpc-id', Values: [vpcId] }] },
+        )) {
+          for (const igw of page.InternetGateways ?? []) {
+            for (const attachment of igw.Attachments ?? []) {
+              if (attachment.State == 'available') {
+                console.log(`Detaching ${igw.InternetGatewayId}`);
+                await throttlingBackOff(() =>
+                  ec2Client.send(
+                    new DetachInternetGatewayCommand({ InternetGatewayId: igw.InternetGatewayId, VpcId: vpcId }),
+                  ),
+                );
+              }
+              console.warn(`${igw.InternetGatewayId} is not attached. Proceeding to delete.`);
+              await throttlingBackOff(() =>
+                ec2Client.send(
+                  new DeleteInternetGatewayCommand({
+                    InternetGatewayId: igw.InternetGatewayId,
+                  }),
+                ),
+              );
+            }
+          }
+        }
+
+        // Retrieve Default VPC Subnets
+        console.log(`Gathering Subnets for VPC ${vpcId}`);
+        for await (const page of paginateDescribeSubnets(
+          { client: ec2Client },
+          { Filters: [{ Name: 'vpc-id', Values: [vpcId] }] },
+        )) {
+          for (const subnet of page.Subnets ?? []) {
+            console.log(`Delete Subnet ${subnet.SubnetId}`);
+            await throttlingBackOff(() =>
+              ec2Client.send(
+                new DeleteSubnetCommand({
+                  SubnetId: subnet.SubnetId,
+                }),
+              ),
+            );
+          }
+        }
+
+        // Delete Routes
+        console.log(`Gathering list of Route Tables for VPC ${vpcId}`);
+        for await (const page of paginateDescribeRouteTables(
+          { client: ec2Client },
+          { Filters: [{ Name: 'vpc-id', Values: [vpcId] }] },
+        )) {
+          for (const routeTableObject of page.RouteTables ?? []) {
+            for (const routes of routeTableObject.Routes ?? []) {
+              if (routes.GatewayId !== 'local') {
+                console.log(`Removing route ${routes.DestinationCidrBlock} from ${routeTableObject.RouteTableId}`);
+                await throttlingBackOff(() =>
+                  ec2Client.send(
+                    new DeleteRouteCommand({
+                      RouteTableId: routeTableObject.RouteTableId,
+                      DestinationCidrBlock: routes.DestinationCidrBlock,
+                    }),
+                  ),
+                );
+              }
+            }
+          }
+        }
+
+        // List and Delete NACLs
+        console.log(`Gathering list of NACLs for VPC ${vpcId}`);
+        for await (const page of paginateDescribeNetworkAcls(
+          { client: ec2Client },
+          { Filters: [{ Name: 'vpc-id', Values: [vpcId] }] },
+        )) {
+          for (const networkAclObject of page.NetworkAcls ?? []) {
+            if (networkAclObject.IsDefault !== true) {
+              console.log(`Deleting Network ACL ID ${networkAclObject.NetworkAclId}`);
+              await throttlingBackOff(() =>
+                ec2Client.send(
+                  new DeleteNetworkAclCommand({
+                    NetworkAclId: networkAclObject.NetworkAclId,
+                  }),
+                ),
+              );
+            } else {
+              console.warn(`${networkAclObject.NetworkAclId} is the default NACL. Ignoring`);
+            }
+          }
+        }
+
+        // List and Delete Security Groups
+        console.log(`Gathering list of Security Groups for VPC ${vpcId}`);
+        for await (const page of paginateDescribeSecurityGroups(
+          { client: ec2Client },
+          { Filters: [{ Name: 'vpc-id', Values: [vpcId] }] },
+        )) {
+          for (const securityGroupObject of page.SecurityGroups ?? []) {
+            if (securityGroupObject.GroupName == 'default') {
+              console.warn(`${securityGroupObject.GroupId} is the default SG. Ignoring`);
+            } else {
+              console.log(`Deleting Security Group Id ${securityGroupObject.GroupId}`);
+              await throttlingBackOff(() =>
+                ec2Client.send(
+                  new DeleteSecurityGroupCommand({
+                    GroupId: securityGroupObject.GroupId,
+                  }),
+                ),
+              );
+            }
+          }
+        }
+        // Once all resources are deleted, delete the VPC.
+        console.log(`Deleting VPC ${vpcId}`);
+        await throttlingBackOff(() => ec2Client.send(new DeleteVpcCommand({ VpcId: vpcId })));
+      }
+
       return {
         PhysicalResourceId: `delete-default-vpc`,
         Status: 'SUCCESS',
