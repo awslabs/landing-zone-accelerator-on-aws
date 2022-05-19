c19196b2fe34cd7398626e1fbdb6655649482a90
Randy Domingo <domrandy@amazon.com>
2022-05-19 01:03:12 +0000

    chore: update copyright for 2022

--- source/packages/@aws-accelerator/config/lib/organization-config.ts
+++ source/packages/@aws-accelerator/config/lib/organization-config.ts
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
  *  with the License. A copy of the License is located at
@@ -10,6 +10,7 @@
  *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
  *  and limitations under the License.
  */
+
 import { throttlingBackOff } from '@aws-accelerator/utils';
 import * as AWS from 'aws-sdk';
 import * as fs from 'fs';
--- source/packages/@aws-accelerator/constructs/lib/aws-ram/get-resource-share-item/index.ts
+++ source/packages/@aws-accelerator/constructs/lib/aws-ram/get-resource-share-item/index.ts
@@ -1,7 +1,5 @@
-import * as AWS from 'aws-sdk';
-
 /**
- *  Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
  *  with the License. A copy of the License is located at
@@ -12,7 +10,9 @@ import * as AWS from 'aws-sdk';
  *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
  *  and limitations under the License.
  */
+
 import { throttlingBackOff } from '@aws-accelerator/utils';
+import * as AWS from 'aws-sdk';
 
 AWS.config.logger = console;
 
