7e597fb56872262748c4b21fb58c90945fbabafb
Jimmy Clem <clemj@amazon.com>
2022-08-08 13:50:53 +0000

    fix(vpcTemplates): VPC templates rework post v1.1.0 merge

--- source/packages/@aws-accelerator/accelerator/lib/stacks/accelerator-stack.ts
+++ source/packages/@aws-accelerator/accelerator/lib/stacks/accelerator-stack.ts
@@ -11,20 +11,25 @@
  *  and limitations under the License.
  */
 
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
 import {
   AccountsConfig,
   DeploymentTargets,
   GlobalConfig,
   IamConfig,
   NetworkConfig,
+  NetworkConfigTypes,
   OrganizationConfig,
   SecurityConfig,
   ShareTargets,
+  VpcConfig,
+  VpcTemplatesConfig,
 } from '@aws-accelerator/config';
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Logger } from '../logger';
+
 import { version } from '../../../../../package.json';
+import { Logger } from '../logger';
 
 export interface AcceleratorStackProps extends cdk.StackProps {
   readonly configDirPath: string;
@@ -76,6 +81,115 @@ export abstract class AcceleratorStack extends cdk.Stack {
 
     // Implicit Deny
     return false;
+  }
+  protected getAccountNamesFromDeploymentTarget(deploymentTargets: DeploymentTargets): string[] {
+    const accountNames: string[] = [];
+
+    // Helper function to add an account to the list
+    const addAccountName = (accountName: string) => {
+      if (!accountNames.includes(accountName)) {
+        accountNames.push(accountName);
+      }
+    };
+
+    for (const ou of deploymentTargets.organizationalUnits ?? []) {
+      if (ou === 'Root') {
+        for (const account of [
+          ...this.props.accountsConfig.mandatoryAccounts,
+          ...this.props.accountsConfig.workloadAccounts,
+        ]) {
+          addAccountName(account.name);
+        }
+      } else {
+        for (const account of [
+          ...this.props.accountsConfig.mandatoryAccounts,
+          ...this.props.accountsConfig.workloadAccounts,
+        ]) {
+          if (ou === account.organizationalUnit) {
+            addAccountName(account.name);
+          }
+        }
+      }
+    }
+
+    for (const account of deploymentTargets.accounts ?? []) {
+      addAccountName(account);
+    }
+
+    return accountNames;
+  }
+
+  protected getAccountIdsFromDeploymentTarget(deploymentTargets: DeploymentTargets): string[] {
+    const accountIds: string[] = [];
+
+    // Helper function to add an account id to the list
+    const addAccountId = (accountId: string) => {
+      if (!accountIds.includes(accountId)) {
+        accountIds.push(accountId);
+      }
+    };
+
+    for (const ou of deploymentTargets.organizationalUnits ?? []) {
+      // debug: processing ou
+      if (ou === 'Root') {
+        for (const account of this.props.accountsConfig.accountIds ?? []) {
+          // debug: accountId
+          addAccountId(account.accountId);
+        }
+      } else {
+        for (const account of [
+          ...this.props.accountsConfig.mandatoryAccounts,
+          ...this.props.accountsConfig.workloadAccounts,
+        ]) {
+          if (ou === account.organizationalUnit) {
+            const accountId = this.props.accountsConfig.getAccountId(account.name);
+            // debug: accountId
+            addAccountId(accountId);
+          }
+        }
+      }
+    }
+
+    for (const account of deploymentTargets.accounts ?? []) {
+      const accountId = this.props.accountsConfig.getAccountId(account);
+      addAccountId(accountId);
+    }
+
+    return accountIds;
+  }
+
+  protected getExcludedAccountIds(deploymentTargets: DeploymentTargets): string[] {
+    const accountIds: string[] = [];
+
+    // Helper function to add an account id to the list
+    const addAccountId = (accountId: string) => {
+      if (!accountIds.includes(accountId)) {
+        accountIds.push(accountId);
+      }
+    };
+
+    if (deploymentTargets.excludedAccounts) {
+      deploymentTargets.excludedAccounts.forEach(account =>
+        addAccountId(this.props.accountsConfig.getAccountId(account)),
+      );
+    }
+
+    return accountIds;
+  }
+
+  protected getVpcAccountIds(vpcItem: VpcConfig | VpcTemplatesConfig): string[] {
+    let vpcAccountIds: string[];
+
+    if (NetworkConfigTypes.vpcConfig.is(vpcItem)) {
+      vpcAccountIds = [this.props.accountsConfig.getAccountId(vpcItem.account)];
+    } else {
+      const excludedAccountIds = this.getExcludedAccountIds(vpcItem.deploymentTargets);
+      vpcAccountIds = this.getAccountIdsFromDeploymentTarget(vpcItem.deploymentTargets).filter(
+        item => !excludedAccountIds.includes(item),
+      );
+    }
+
+    return vpcAccountIds;
   }
 
   protected getAccountIdsFromShareTarget(shareTargets: ShareTargets): string[] {
