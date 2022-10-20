cc245e3b93073a854b8f89609e144c7cd51510db
Nagmesh Kumar <nagmeshk@amazon.com>
2022-10-20 20:16:47 +0000

    fix(logging): transform CloudWatch logs data to allow query from athena

--- source/packages/@aws-accelerator/constructs/lib/aws-firehose/firehose-record-processing/index.ts
+++ source/packages/@aws-accelerator/constructs/lib/aws-firehose/firehose-record-processing/index.ts
@@ -25,7 +25,8 @@ AWS.config.logger = console;
 
 export async function handler(event: AWSLambda.FirehoseTransformationEvent) {
   // Retrieve operating region that stack is ran
-  console.log(`Processing firehose records...`);
+  console.log(event);
+  // console.log(`Processing firehose records...`);
 
   const firehoseRecordsOutput: AWSLambda.FirehoseTransformationResult = { records: [] };
 
@@ -64,13 +65,16 @@ async function processFirehoseInputRecord(firehoseRecord: AWSLambda.FirehoseTran
     const firehoseTimestamp = new Date(firehoseRecord.approximateArrivalTimestamp);
     const prefixes = await getDatePrefix(serviceName, firehoseTimestamp);
 
+    // transform data to flatten json schema
+    const transformedData = await getTransformedData(jsonParsedPayload);
+
     // these are mandatory prefixes for firehose payload
     const partitionKeys = {
       dynamicPrefix: prefixes,
     };
     const firehoseReturnResult: AWSLambda.FirehoseTransformationResultRecord = {
       recordId: firehoseRecord.recordId,
-      data: firehoseRecord.data,
+      data: transformedData,
       result: 'Ok',
     };
 
@@ -152,4 +156,38 @@ async function getDatePrefix(serviceName: string | null, inputTimestamp: Date) {
   calculatedPrefix += `/`;
 
   return calculatedPrefix;
+}
+
+type singleRowItem = {
+  messageType: string;
+  owner: string;
+  logGroup: string;
+  logStream: string;
+  subscriptionFilters: [string];
+  logEvents: [
+    {
+      id: string;
+      timestamp: bigint;
+      message: string;
+    },
+  ];
+};
+
+async function getTransformedData(jsonParsedPayload: singleRowItem) {
+  const jsonFormattedPayload = [];
+  for (const logEvent of jsonParsedPayload.logEvents) {
+    const singleRow = {
+      // making keys lower case for json serializer in firehose
+      messagetype: jsonParsedPayload.messageType,
+      owner: jsonParsedPayload.owner,
+      loggroup: jsonParsedPayload.logGroup,
+      subscriptionfilters: jsonParsedPayload.subscriptionFilters.join(','),
+      logeventsid: logEvent.id,
+      logeventstimestamp: logEvent.timestamp,
+      logeventsmessage: logEvent.message,
+    };
+    jsonFormattedPayload.push(JSON.stringify(singleRow));
+  }
+  const encodePayload = Buffer.from(jsonFormattedPayload.join('\n')).toString('base64');
+  return encodePayload;
 }
