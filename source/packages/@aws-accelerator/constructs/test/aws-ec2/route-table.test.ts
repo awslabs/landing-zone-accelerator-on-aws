6ee874369d27e961c539340c6e493824a7c0cb17
Dustin Hickey <hickeydh@amazon.com>
2022-12-19 22:32:44 -0800

    chore: added tests for files

--- source/packages/@aws-accelerator/constructs/test/aws-ec2/route-table.test.ts
+++ source/packages/@aws-accelerator/constructs/test/aws-ec2/route-table.test.ts
@@ -13,14 +13,14 @@
 
 import * as cdk from 'aws-cdk-lib';
 import { RouteTable } from '../../lib/aws-ec2/route-table';
-import { Vpc } from '../../lib/aws-ec2/vpc';
+import { NatGateway, Vpc, Subnet } from '../../lib/aws-ec2/vpc';
 import { snapShotTest } from '../snapshot-test';
 
 const testNamePrefix = 'Construct(RouteTable): ';
 
 //Initialize stack for snapshot test and resource configuration test
 const stack = new cdk.Stack();
-
+const key = new cdk.aws_kms.Key(stack, 'testKey');
 const vpc = new Vpc(stack, 'TestVpc', {
   name: 'Test',
   ipv4CidrBlock: '10.0.0.0/16',
@@ -28,14 +28,43 @@ const vpc = new Vpc(stack, 'TestVpc', {
   enableDnsHostnames: false,
   enableDnsSupport: true,
   instanceTenancy: 'default',
+  virtualPrivateGateway: {
+    asn: 65000,
+  },
 });
 
-new RouteTable(stack, 'RouteTable', {
+const rt = new RouteTable(stack, 'RouteTable', {
   name: 'TestRouteTable',
   vpc: vpc,
   tags: [{ key: 'Test-Key', value: 'Test-Value' }],
 });
 
+const subnet = new Subnet(stack, 'test-subnet', {
+  name: 'test-subnet',
+  routeTable: rt,
+  vpc,
+  availabilityZone: 'a',
+  ipv4CidrBlock: '10.0.2.0/24',
+});
+
+const ngw = new NatGateway(stack, 'ngw', { name: 'ngw', subnet });
+
+const tgwAttachment = new cdk.aws_ec2.CfnTransitGatewayAttachment(stack, 'tgwAttachment', {
+  subnetIds: ['subnet-123'],
+  transitGatewayId: 'tgw-12324',
+  vpcId: vpc.vpcId,
+});
+
+rt.addTransitGatewayRoute('tg-route', 'tgw-1234', tgwAttachment, undefined, 'pl-1234', key, 10);
+rt.addTransitGatewayRoute('tg', 'tg-1234', tgwAttachment, '10.0.5.0/24', undefined, key, 10);
+rt.addNatGatewayRoute('testNgwRoute', ngw.natGatewayId, '10.0.3.0/24', undefined, key, 10);
+rt.addNatGatewayRoute('test2NgwRoute', ngw.natGatewayId, undefined, 'pl-1234', key, 10);
+rt.addInternetGatewayRoute('testIgwRoute', '0.0.0.0/0', undefined, key, 10);
+rt.addInternetGatewayRoute('testIgwRoute2', undefined, 'pl-1234', key, 10);
+rt.addVirtualPrivateGatewayRoute('testVgwRoute', '10.0.30./24', undefined, key, 10);
+rt.addVirtualPrivateGatewayRoute('testVgw2Route', undefined, 'pl-1234', key, 10);
+rt.addGatewayAssociation('internetGateway');
+rt.addGatewayAssociation('virtualPrivateGateway');
 /**
  * RouteTable construct test
  */
