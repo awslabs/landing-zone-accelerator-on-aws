22a51000e15f877fb252224301a7e04249800130
Eric Waxler <erwaxler@amazon.com>
2023-06-28 20:29:10 +0000

    fix(scps): scpRevertChanges should use accelerator prefix

--- source/packages/@aws-accelerator/accelerator/lib/stacks/accounts-stack.ts
+++ source/packages/@aws-accelerator/accelerator/lib/stacks/accounts-stack.ts
@@ -464,6 +464,7 @@ export class AccountsStack extends AcceleratorStack {
     if (props.securityConfig.centralSecurityServices?.scpRevertChangesConfig?.enable) {
       this.logger.info(`Creating resources to revert modifications to scps`);
       new RevertScpChanges(this, 'RevertScpChanges', {
+        acceleratorPrefix: props.prefixes.accelerator,
         configDirPath: props.configDirPath,
         homeRegion: props.globalConfig.homeRegion,
         kmsKeyCloudWatch: this.cloudwatchKey,
