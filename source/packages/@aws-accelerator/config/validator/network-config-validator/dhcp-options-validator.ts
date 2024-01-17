993cb7103c9bd1d897228f7a356c8c15ec73b19b
Jimmy Clem <clemj@amazon.com>
2024-01-17 21:53:02 +0000

    feat(networking): add IPv6 support for DHCP options sets

--- source/packages/@aws-accelerator/config/validator/network-config-validator/dhcp-options-validator.ts
+++ source/packages/@aws-accelerator/config/validator/network-config-validator/dhcp-options-validator.ts
@@ -13,6 +13,11 @@
 import { NetworkConfig, DhcpOptsConfig } from '../../lib/network-config';
 import { NetworkValidatorFunctions } from './network-validator-functions';
 
+interface IpAddresses {
+  ipv4: string[];
+  ipv6: string[];
+}
+
 /**
  * Class to validate DHCP options sets
  */
@@ -101,30 +106,86 @@ export class DhcpOptionsValidator {
    * @param errors
    */
   private validateIpAddresses(set: DhcpOptsConfig, helpers: NetworkValidatorFunctions, errors: string[]) {
-    // Validate IP addresses are valid
-    const ips = [...(set.domainNameServers ?? []), ...(set.netbiosNameServers ?? []), ...(set.ntpServers ?? [])];
-    ips.forEach(ip => {
-      if (ip !== 'AmazonProvidedDNS' && !helpers.isValidIpv4(ip)) {
+    //
+    // Validate DNS IP addresses
+    set.domainNameServers?.forEach(dnsIp => {
+      if (dnsIp !== 'AmazonProvidedDNS' && !helpers.isValidIpv4(dnsIp) && !helpers.isValidIpv6(dnsIp)) {
         errors.push(
-          `[DHCP options set ${set.name}]: IP address "${ip}" is invalid. Values must be either a valid IPv4 address or AmazonProvidedDNS`,
+          `[DHCP options set ${set.name}]: IP address "${dnsIp}" is invalid. Values for domainNameServers must be either a valid IPv4/v6 address or AmazonProvidedDNS`,
         );
       }
     });
+    //
+    // Validate NTP IP addresses
+    set.ntpServers?.forEach(ntpIp => {
+      if (!helpers.isValidIpv4(ntpIp) && !helpers.isValidIpv6(ntpIp)) {
+        errors.push(
+          `[DHCP options set ${set.name}]: IP address "${ntpIp}" is invalid. Values for ntpServers must be a valid IPv4/v6 address`,
+        );
+      }
+    });
+    //
+    // Validate NetBIOS name servers (only supports IPv4)
+    set.netbiosNameServers?.forEach(netBiosIp => {
+      if (!helpers.isValidIpv4(netBiosIp)) {
+        errors.push(
+          `[DHCP options set ${set.name}]: IP address "${netBiosIp}" is invalid. Values for netbiosNameServers must be a valid IPv4 address`,
+        );
+      }
+    });
+    //
+    // Filter and store IPs
+    const domainNameServers: IpAddresses = {
+      ipv4: set.domainNameServers?.filter(dnsIpv4 => helpers.isValidIpv4(dnsIpv4)) ?? [],
+      ipv6: set.domainNameServers?.filter(dnsIpv6 => helpers.isValidIpv6(dnsIpv6)) ?? [],
+    };
+    const ntpServers: IpAddresses = {
+      ipv4: set.ntpServers?.filter(ntpIpv4 => helpers.isValidIpv4(ntpIpv4)) ?? [],
+      ipv6: set.ntpServers?.filter(ntpIpv6 => helpers.isValidIpv6(ntpIpv6)) ?? [],
+    };
+    const netbiosNameServers: IpAddresses = {
+      ipv4: set.netbiosNameServers?.filter(netBiosIpv4 => helpers.isValidIpv4(netBiosIpv4)) ?? [],
+      ipv6: [],
+    };
+    //
+    // Validate length of IP addresses
+    this.validateIpAddressLength(domainNameServers, ntpServers, netbiosNameServers, set.name, errors);
+  }
 
-    // Validate number of servers defined
-    if (set.domainNameServers && set.domainNameServers.length > 4) {
+  /**
+   * Validate length of each IP address type
+   * @param domainNameServers
+   * @param ntpServers
+   * @param netbiosNameServers
+   * @param setName
+   * @param errors
+   */
+  private validateIpAddressLength(
+    domainNameServers: IpAddresses,
+    ntpServers: IpAddresses,
+    netbiosNameServers: IpAddresses,
+    setName: string,
+    errors: string[],
+  ) {
+    //
+    // Validate total length of DNS IPs
+    if (domainNameServers.ipv4.length > 4 || domainNameServers.ipv6.length > 4) {
       errors.push(
-        `[DHCP options set ${set.name}]: domainNameServers has ${set.domainNameServers.length} servers defined. A maximum of 4 servers may be defined`,
+        `[DHCP options set ${setName}]: maximum domainNameServers server address threshold breached -- IPv4: ${domainNameServers.ipv4.length} IPv6: ${domainNameServers.ipv6.length}. No more than 4 servers of each IP version type may be defined`,
       );
     }
-    if (set.netbiosNameServers && set.netbiosNameServers.length > 4) {
+    //
+    // Validate total length of NTP IPs
+    if (ntpServers.ipv4.length > 4 || ntpServers.ipv6.length > 4) {
       errors.push(
-        `[DHCP options set ${set.name}]: netbiosNameServers has ${set.netbiosNameServers.length} servers defined. A maximum of 4 servers may be defined`,
+        `[DHCP options set ${setName}]: maximum ntpServers server address threshold breached -- IPv4: ${ntpServers.ipv4.length} IPv6: ${ntpServers.ipv6.length}. No more than 4 servers of each IP version type may be defined`,
       );
     }
-    if (set.ntpServers && set.ntpServers.length > 4) {
+    //
+    // Validate total length of NetBIOS IPs
+    if (netbiosNameServers.ipv4.length > 4) {
       errors.push(
-        `[DHCP options set ${set.name}]: ntpServers has ${set.ntpServers.length} servers defined. A maximum of 4 servers may be defined`,
+        `[DHCP options set ${setName}]: netbiosNameServers has ${netbiosNameServers.ipv4.length} IPv4 servers defined. A maximum of 4 servers may be defined`,
       );
     }
   }
