4d96dd5f416675d0e977f702123422dc72e6e603
Partha Debnath <padebnat@amazon.com>
2024-05-06 13:52:50 +0000

    chore(logging): static code scan for logging

---
+++ source/log-scanner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+BRed='\033[1;31m' 
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+cd ../
+files=$(git diff --staged --name-only)
+for file in $files; do
+    lines=$(awk '/console.log\(/{ print NR; }' $file)
+    for line in $lines; do
+      echo -e "${BRed}Warning!!! - ${NC}${RED}${file}${NC} has ${BLUE}console logging${NC} in line ${RED}${line}${NC}, review the logging statement."
+    done
+done
