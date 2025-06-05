acce24a1b660d345b93a8feca4dbc3b64d5d5a4d
Wenjie Hou <houw@amazon.de>
2025-06-05 21:29:21 +0000

    feat(networking): add metadata for resources in route table stack

---
+++ source/packages/@aws-accelerator/accelerator/test/unit-tests/network/network-vpc-stack.test.ts
@@ -0,0 +1,58 @@
+import { describe } from '@jest/globals';
+import { snapShotTest } from '../../snapshot-test';
+import { Create } from '../../accelerator-test-helpers';
+import { AcceleratorStage } from '../../../lib/accelerator-stage';
+import path from 'path';
+import { readFileSync } from 'fs';
+import { Template } from 'aws-cdk-lib/assertions';
+import { Stack } from 'aws-cdk-lib';
+
+describe('NetworkVpcStack tests', () => {
+  let originalCwd: string;
+  let testDir: string;
+  let existingTemplate: Template;
+  let newTemplate: Template;
+
+  beforeAll(() => {
+    originalCwd = process.cwd();
+    testDir = path.join(process.cwd(), 'test');
+    process.chdir(testDir);
+
+    const existingTemplateString = readFileSync(
+      'cfn-templates/666666666666/us-east-1/AWSAccelerator-NetworkVpcStack-666666666666-us-east-1.json',
+    ).toString();
+    existingTemplate = Template.fromString(existingTemplateString);
+
+    const stack = Create.stack(`Network-us-east-1`, {
+      stage: AcceleratorStage.NETWORK_VPC,
+      configFolderName: 'network-refactor',
+    }) as Stack;
+    newTemplate = Template.fromStack(stack);
+  });
+
+  afterAll(() => {
+    process.chdir(originalCwd);
+  });
+
+  snapShotTest(
+    'Construct(NetworkVpcStack): ',
+    Create.stackProvider(`Network-us-east-1`, {
+      stage: AcceleratorStage.NETWORK_VPC,
+      configFolderName: 'network-refactor',
+    }),
+  );
+
+  describe('Route Table Resources', () => {
+    it.each([
+      'AWS::EC2::RouteTable',
+      'Custom::PrefixListRoute',
+      'AWS::EC2::Route',
+      'AWS::EC2::LocalGatewayRouteTableVPCAssociation',
+      'AWS::EC2::GatewayRouteTableAssociation',
+    ])('Should contains same amount of %s', (type: string) => {
+      const oldTemplateCount = Object.keys(existingTemplate.findResources(type)).length;
+      const newTemplateCount = Object.keys(newTemplate.findResources(type)).length;
+      expect(oldTemplateCount).toEqual(newTemplateCount);
+    });
+  });
+});
